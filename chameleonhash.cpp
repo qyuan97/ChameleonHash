f7d62bfb5401754fe175395af8186ad7f474e8b5
Elric <xieqyhit@gmail.com>
2022-01-26 21:07:06 +0800

    update forge

--- chameleonhash.cpp
+++ chameleonhash.cpp
@@ -69,8 +69,26 @@ CryptoPP::Integer chameleonHash(std::string msg, CHPublicKey pk, CHSecretKey sk,
 
 // TODO forge
 // CH=g^m*h^r =g^m'*h^r' mod p，可得m+rx=m'+r'x mod q，继而可计算出r'=(m-m'+rx)*x^(-1) mod q
-CryptoPP::Integer forge(std::string new_msg){
+CryptoPP::Integer forge(std::string ori_msg, std::string new_msg, CHPublicKey pk, CHSecretKey sk, CryptoPP::Integer r){
     CryptoPP::Integer new_r;
+
+    std::string ori_digest;
+    CryptoPP::SHA3_256 ori_hash;
+    ori_hash.Update((const CryptoPP::byte*)ori_msg.data(), ori_msg.size());
+    ori_digest.resize(ori_hash.DigestSize());
+    ori_hash.Final((CryptoPP::byte*)&ori_digest[0]);
+
+    std::string new_digest;
+    CryptoPP::SHA3_256 new_hash;
+    new_hash.Update((const CryptoPP::byte*)new_msg.data(), new_msg.size());
+    new_digest.resize(new_hash.DigestSize());
+    new_hash.Final((CryptoPP::byte*)&new_digest[0]);
+
+    CryptoPP::Integer m(ori_digest.c_str());
+    CryptoPP::Integer new_m(new_digest.c_str());
+
+    new_r = (m - new_m + r * sk.sk) / sk.sk % pk.q;
+
     return new_r;
 }
 
@@ -78,6 +96,7 @@ int main(int argc, const char* argv[]){
     auto [pk, sk] = Keygen(1, 1024);
     CryptoPP::AutoSeededRandomPool prng;
     CryptoPP::Integer r(prng, 0, pk.q);
+
     std::string s = "yes";
     clock_t start=clock();
     CryptoPP::Integer ch = chameleonHash(s, pk, sk, r);
@@ -85,6 +104,16 @@ int main(int argc, const char* argv[]){
     std::cout << "chameleon hash: " << ch << std::endl;
     double Times = (double)(finish-start) / CLOCKS_PER_SEC;
     std::cout << "generation time: " << Times << "s" << std::endl;
+
+    std::string new_s = "no";
+    clock_t forge_start=clock();
+    CryptoPP::Integer new_r = forge(s, new_s, pk, sk, r);
+    clock_t forge_finish=clock();
+    std::cout << "r: " << r << std::endl;
+    std::cout << "forge: " << new_r << std::endl;
+    double forge_Times = (double)(forge_finish-forge_start) / CLOCKS_PER_SEC;
+    std::cout << "forge time: " << forge_Times << "s" << std::endl;
+
     return 0;
 }
 
